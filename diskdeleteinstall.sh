fb9aa63d0229442a8cdfdc72d007fab4b8406866
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2023-01-25 19:03:39 +0100

    Update diskdeleteinstall.sh

--- diskdeleteinstall.sh
+++ diskdeleteinstall.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 # Make blink1 blink orange for 10 seconds before erasing
-blink1-tool --delay 1000 --rgb 255,136,0  --blink 10
 
-blink1-tool --red
 
-sleep 5
+##blink1-tool --delay 1000 --rgb 255,136,0  --blink 10
+
+##blink1-tool --red
+
+##sleep 5
 
 # Use DD to securely erase the disk 7 times with random numbers
 ##for i in {1..7}; do
