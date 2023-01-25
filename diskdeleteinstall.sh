4f1fca1c856d37ec630077f80594e5e02c2a5243
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2023-01-25 15:18:43 +0100

    Update diskdeleteinstall.sh

--- diskdeleteinstall.sh
+++ diskdeleteinstall.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 # Make blink1 blink orange for 10 seconds before erasing
-blink1-tool --delay 1000 --rgb 255,136,0  --blink 10
+##blink1-tool --delay 1000 --rgb 255,136,0  --blink 10
 
 # Use DD to securely erase the disk 7 times
-for i in {1..7}; do 
-    sudo dd if=/dev/urandom of=/dev/sda1 bs=1M status=progress
+for i in {1..7}; do
+    blink1-tool --red
+    dd if=/dev/urandom of=/dev/sda1 bs=1M status=progress
     if  [$? -ne 0 ]; then
         #Make blink1 blink red if there is an error
         blink1-tool --delay 1000 --red  --blink 0
