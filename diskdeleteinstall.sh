9730979e927767fff95a1c48cc7ea0eede0934dd
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2023-01-25 15:25:32 +0100

    Update diskdeleteinstall.sh

--- diskdeleteinstall.sh
+++ diskdeleteinstall.sh
@@ -6,7 +6,6 @@ blink1-tool --red
 
 # Use DD to securely erase the disk 7 times
 for i in {1..7}; do
-    #blink1-tool --red
     dd if=/dev/urandom of=/dev/sda1 bs=1M status=progress
     if  [$? -ne 0 ]; then
         #Make blink1 blink red if there is an error
