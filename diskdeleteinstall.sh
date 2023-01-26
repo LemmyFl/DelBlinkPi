ff002d5f13ee4b65f98a3b07dbb68f1afc4e3f97
LemmyFl <lennartheidtmann2015@gmail.com>
2023-01-26 20:15:31 +0100

    1

--- diskdeleteinstall.sh
+++ diskdeleteinstall.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-echo "USB device added at $(date)" >> "sudo curl https://raw.githubusercontent.com/LemmyFl/DelBlinkPi/main/diskdeleteinstall.sh | bash"
+##echo "USB device added at $(date)" >> "sudo curl https://raw.githubusercontent.com/LemmyFl/DelBlinkPi/main/diskdeleteinstall.sh | bash"
 
 # Make blink1 flash orange for 10 seconds
-blink1-tool --delay 1000 --rgb 255,136,0  --blink 10
+##blink1-tool --delay 1000 --rgb 255,136,0  --blink 10
 
 # Use DD to securely erase the disk 7 times with random numbers
-for _ in {1..7}; do
+##for _ in {1..7}; do
     # Make blink1 flash orange while secure erase, if secure erase fails blink1 flashs red
-    blink1-tool --delay 1000 --rgb 255,136,0  --blink 0 | sudo dd if=/dev/zero of=/dev/sdX bs=1M status=progress ||     blink1-tool --delay 1000 --red  --blink 0
-done
+    blink1-tool --delay 1000 --rgb 255,136,0  --blink 0 | sudo dd if=/dev/urandom of=/dev/sdX bs=1M status=progress ||     blink1-tool --delay 1000 --red  --blink 0
+##done
 
 # Make blink1 green when erase is completed successfully
 blink1-tool --green
-
-
-test123
