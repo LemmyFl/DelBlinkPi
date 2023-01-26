c2f1b2e2735a3dffc1aec7dabac6c60a9539d7f1
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2023-01-26 15:07:08 +0100

    Update diskdeleteinstall.sh

--- diskdeleteinstall.sh
+++ diskdeleteinstall.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Update the 
-##sudo apt update
-
 # Make blink1 flash orange for 10 seconds
 blink1-tool --delay 1000 --rgb 255,136,0  --blink 10
 
+
 # Use DD to securely erase the disk 7 times with random numbers
 for _ in {1..7}; do
-    sudo dd if=/dev/zero of=/dev/sda bs=1M status=progress || blink1-tool --delay 1000 --red  --blink 0
+    blink1-tool --delay 1000 --rgb 255,136,0  --blink 0 | sudo dd if=/dev/zero of=/dev/sda bs=1M status=progress ||     blink1-tool --delay 1000 --red  --blink 0
 done
 
 # Make blink1 green when erase is completed successfully
